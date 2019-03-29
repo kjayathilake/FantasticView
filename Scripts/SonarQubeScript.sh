0c07009cd66770dff89e1bab00f9e855aa40214f
Krishantha Jayathilake <krishantha.jayathilake@pearson.com>
2019-03-29 20:38:01 +0530

    added sonar qube scripts

---
+++ Scripts/SonarQubeScript.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Running sonarqube analysis"
+PROPERTIES="${XCS_SOURCE_DIR}/Test/sonar-project.properties"
+INFOPLIST="${XCS_SOURCE_DIR}/Test/Writer/Info.plist"
+buildNumber=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" $INFOPLIST)
+buildVersion=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" $INFOPLIST)
+
+echo "sonar.host.url=$1" >> $PROPERTIES
+echo "sonar.login=$2" >> $PROPERTIES
+if [ -z "$3" ]
+then
+	echo "sonar.password=$3" >> $PROPERTIES
+fi
+echo "sonar.projectVersion=$buildVersion.$buildNumber.${XCS_INTEGRATION_NUMBER}" >> $PROPERTIES
+sh "run-sonar-swift.sh"
